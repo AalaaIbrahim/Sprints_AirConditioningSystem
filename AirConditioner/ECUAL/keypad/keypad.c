86dc90240617d4303445589e6e8c30c78db3dd40
sharpel malak <62040665+sharpelmalak@users.noreply.github.com>
2023-04-17 21:30:18 +0200

    adding Keypad driver

--- AirConditioner/ECUAL/keypad/keypad.c
+++ AirConditioner/ECUAL/keypad/keypad.c
@@ -8,6 +8,7 @@
  * 													Includes
  ************************************************************************************************************/
 #include "../../Common/STD_Types.h"
+#include "../../Common/BIT_Math.h"
 
 #include "../../MCAL/dio/dio_interface.h"
 
@@ -15,7 +16,103 @@
 /*************************************************************************************************************
  * 												Global Variables
  ************************************************************************************************************/
-
+uint8_t keys_arr [] = {'+','-','=','~','0','6','7','8','9'};
 /*************************************************************************************************************
  * 											Function Implementation
  ************************************************************************************************************/
+
+void KEYPAD_init()
+{
+	KEYPAD_DIR &=~(1<<C1) | (1<<C2) | (1<<C3); // COLUMNS ARE INPUTS
+	KEYPAD_DIR |= (1<<R1) | (1<<R2) | (1<<R3); // ROWS ARE OUTPUTS	
+}
+EN_KEYPAD_BTNS checkR1()
+{
+	
+	KEYPAD_PORT |= (1<<C1) | (1<<C2) |(1<<C3) | (1<<R2) | (1<<R3);
+	KEYPAD_PORT &=~ (1<<R1);
+	if(GET_BIT(PINC,C1) == 0)
+	{
+		
+		while (GET_BIT(PINC,C1) == 0);
+		return KEY_INCREAMENT;
+	}
+	if(GET_BIT(PINC,C2) == 0)
+	{
+		while (GET_BIT(PINC,C2) == 0);
+		return KEY_DECREAMENT;
+	}
+	if(GET_BIT(PINC,C3) == 0)
+	{
+		
+		while (GET_BIT(PINC,C3) == 0);
+		return KEY_SET;
+	}
+	return KEY_NOTHING;
+	
+}
+EN_KEYPAD_BTNS checkR2()
+{
+	KEYPAD_PORT |= (1<<C1) | (1<<C2) |(1<<C3) | (1<<R1) | (1<<R3);
+	KEYPAD_PORT &=~ (1<<R2);
+	if(GET_BIT(PINC,C1) == 0)
+	{
+		
+		while (GET_BIT(PINC,C1) == 0);
+		return KEY_ADJUST;
+	}
+	if(GET_BIT(PINC,C2) == 0)
+	{
+		while (GET_BIT(PINC,C2) == 0);
+		return KEY_RESET;
+	}
+	if(GET_BIT(PINC,C3) == 0)
+	{
+		
+		while (GET_BIT(PINC,C3) == 0);
+		return KEY_6;
+	}
+	return KEY_NOTHING;
+	
+}
+EN_KEYPAD_BTNS checkR3()
+{
+	KEYPAD_PORT |= (1<<C1) | (1<<C2) |(1<<C3) | (1<<R1) | (1<<R2);
+	KEYPAD_PORT &=~ (1<<R3);
+	if(GET_BIT(PINC,C1) == 0)
+	{
+		
+		while (GET_BIT(PINC,C1) == 0);
+		return KEY_7;
+	}
+	if(GET_BIT(PINC,C2) == 0)
+	{
+		while (GET_BIT(PINC,C2) == 0);
+		return KEY_8;
+	}
+	if(GET_BIT(PINC,C3) == 0)
+	{
+		
+		while (GET_BIT(PINC,C3) == 0);
+		return KEY_9;
+	}
+	return KEY_NOTHING;
+	
+}
+
+EN_KEYPAD_BTNS GetButton()
+{
+	EN_KEYPAD_BTNS PRESSED = KEY_NOTHING;
+	while(PRESSED == KEY_NOTHING)
+	{
+		PRESSED = checkR1();
+		if(PRESSED != KEY_NOTHING) return PRESSED;
+		PRESSED = checkR2();
+		if(PRESSED != KEY_NOTHING) return PRESSED;
+		PRESSED = checkR3();
+		if(PRESSED != KEY_NOTHING) return PRESSED;
+		return KEY_NOTHING;
+	}
+	
+	return KEY_NOTHING;
+}
--- AirConditioner/ECUAL/keypad/keypad.h
+++ AirConditioner/ECUAL/keypad/keypad.h
@@ -2,7 +2,7 @@
  * keypad.h
  *
  * Created: 4/17/2023 4:06:37 AM
- *  Author: ME
+ *  Author: Sharpel
  */ 
 
 
@@ -13,16 +13,78 @@
 /*************************************************************************************************************
  * 													Macros
  ************************************************************************************************************/
+#define R1     2
+#define R2     3
+#define R3     4
+#define C1     5
+#define C2     6
+#define C3     7
 
+
+#define KEYPAD_PORT PORTC
+#define KEYPAD_DIR  DDRC
+#define KEYPAD_PIN  PINC
 
 /*************************************************************************************************************
  * 											User-defined data types
  ************************************************************************************************************/
-
+typedef enum EN_KEYPAD_BTNS
+{
+	KEY_INCREAMENT=0,
+	KEY_DECREAMENT,
+	KEY_SET,
+	KEY_ADJUST,
+	KEY_RESET,
+	KEY_6,
+	KEY_7,
+	KEY_8,
+	KEY_9,
+	KEY_NOTHING
+	
+	}EN_KEYPAD_BTNS;
 
 /*************************************************************************************************************
  * 											  Function Prototypes
  ************************************************************************************************************/
 
 
+/*************************************************************************************************************
+ *
+ Name : KEYPAD_init() 
+ Description : This Function Initializes keypad pins (Rows are outputs & Columns are inputs).
+ ARGS : void
+ return : void   
+ ************************************************************************************************************/
+
+void KEYPAD_init(void);
+
+
+/*************************************************************************************************************
+ *
+ Name : KEYPAD_GetButton 
+ Description : This Function loops over other three functions (Checks (R1,R2,R3)).
+ ARGS : void
+ return : the pressed key or Nothing pressed   
+ ************************************************************************************************************/
+
+EN_KEYPAD_BTNS KEYPAD_GetButton(void);
+/*************************************************************************************************************
+ *
+ Name : KEYPAD_checkR1 ,  KEYPAD_checkR2, KEYPAD_checkR3
+ Description : functions are checking the entire row if it pressed or not.
+ ARGS : void
+ return : the pressed key or Nothing pressed   
+ ************************************************************************************************************/
+
+EN_KEYPAD_BTNS KEYPAD_checkR1(void);
+EN_KEYPAD_BTNS KEYPAD_checkR2(void);
+EN_KEYPAD_BTNS KEYPAD_checkR3(void);
 #endif /* KEYPAD_H_ */
+
+
+
+
+
+
+
+
